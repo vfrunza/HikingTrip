ef81e7febe25c27915e0930ace3b87dd8ed83979
vfrunza <victor.frunza96@gmail.com>
2021-10-11 09:55:09 -0700

    Initial Commit

---
+++ Hiking Trails/Hiking_TrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hiking_TrailsApp.swift
+//  Hiking Trails
+//
+//  Created by Victor Frunza on 2021-10-11.
+//
+
+import SwiftUI
+
+@main
+struct Hiking_TrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
